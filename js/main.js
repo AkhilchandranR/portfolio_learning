cd3e674431ff7dd40b34ce5657e7508bc85df7b8
AkhilchandranR <akhilchandran771@gmail.com>
2021-07-19 11:40:57 +0530

    responsive complete, scroll button done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 window.addEventListener('scroll',function(){
+    const scrollToTopButton = document.querySelector('.btnScrollToTop');
     let header = document.querySelector('nav');
     let windowPosition = window.scrollY;
-    console.log(windowPosition)
     header.classList.toggle('scrolling-active',windowPosition>0);
+    scrollToTopButton.classList.toggle('displayScrollbtn',windowPosition>245);
+
+});
+//scrolltotopbutton properties
+const scrollToTopButton = document.querySelector('.btnScrollToTop');
+scrollToTopButton.addEventListener('click',function(){
+    window.scrollTo({
+        top:0,
+        left:0,
+        behavior: "smooth"
+    });
 });
